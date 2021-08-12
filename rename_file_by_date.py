7e9ff442f324757c940732481988ec3adea31b29
Hartmut Frenzel <hartmut.frenzel@noaa.gov>
2021-08-12 15:30:49 -0700

    Complete rewrite for style; new options -T and -v

--- rename_file_by_date.py
+++ rename_file_by_date.py
@@ -1,49 +1,71 @@
 #!/usr/bin/env python
 #
-# rename_file_by_date.py
+# rename_file_by_date.py: Simple utility script that renames or copies
+# one or more files by appending the date (and optionally time)
+# of its creation to the original name.
 #
-# H. Frenzel, School of Oceanography, University of Washington
+# H. Frenzel, CICOES, University of Washington // NOAA-PMEL
 #
-# First version: March 3, 2020
+# Latest version: August 12, 2021
+#
+# First version:  March 3, 2020
 
-from __future__ import print_function
-
-from sys import argv
+import argparse
 import os
-from optparse import OptionParser
-import systools
+import shutil
+
 from datetime import datetime
-from systools import print_execute_command
 
-parser = OptionParser()
-parser.add_option("-c", "--copy", action="store_true",
-                  dest="create_copy", default=False,
-                  help="copy file instead of renaming it")
-parser.add_option("-t", "--test", action="store_false",
-                  dest="run_cmd", default=True,
-                  help="show commands without executing them")
 
-# note that in "args" the first element is 0, not 1 as in original argv!
-(options, args) = parser.parse_args()
+def parse_input_args():
+    '''Parse the command line arguments and return them as an object.'''
+    parser = argparse.ArgumentParser()
 
-op = 'mv'
-if options.create_copy:
-    op = 'cp -p'
-    
-if len(args) < 1:
-    print("\nError: rename_file_by_date.py [-t] file(s)\n\n")
-    os.system(argv[0] + ' -h')
-    exit(1)
+    # required argument:
+    parser.add_argument('filename_in', nargs='+',
+                        help='name of the input file')
+    # options:
+    parser.add_argument('-c', '--copy', action='store_true',
+                        dest='create_copy', default = False,
+                        help='copy file instead of renaming it')
+    parser.add_argument('-t', '--test', action='store_true',
+                        dest='test_only', default=False,
+                        help='show commands without executing them')
+    parser.add_argument('-T', '--time', action='store_true',
+                        dest='add_time', default=False,
+                        help='add time of file creation to new file name')
+    parser.add_argument('-v', '--verbose', action='store_true',
+                        dest='verbose', default=False,
+                        help='show commands')
+    args = parser.parse_args()
+    return args
 
-files = args
-for file in files:
-    if not os.path.isfile(file):
-        print(file + ' is not a file!')
-        continue # keep going, try the other files
+
+def process_file(filename, args):
+    '''Rename or copy the file with the given name by adding date
+    and optionally time to its name.'''
+    if not os.path.isfile(filename):
+        print('{0:s} is not a file!'.format(filename))
     else:
-        mtime = os.path.getmtime(file)
-        date = datetime.fromtimestamp(mtime).strftime('%b_%d_%Y')
-        file_new = file + '.' + date
-        cmd = '{0:s} {1:s} {2:s}'.format(op, file, file_new)
-        print_execute_command(cmd, options.run_cmd)
+        mtime = os.path.getmtime(filename)
+        if args.add_time:
+            date = datetime.fromtimestamp(mtime).strftime('%b_%d_%Y_%H:%M:%S')
+        else: # use date only
+            date = datetime.fromtimestamp(mtime).strftime('%b_%d_%Y')
+        new_file = '{0:s}_{1:s}'.format(filename, date)    
+        if args.create_copy:
+            if args.test_only or args.verbose:
+                print('cp {0:s} {1:s}'.format(filename, new_file))
+            if not args.test_only:    
+                shutil.copy2(filename, new_file)
+        else:
+            if args.test_only or args.verbose:
+                print('mv {0:s} {1:s}'.format(filename, new_file))
+            if not args.test_only:    
+                shutil.move(filename, new_file)
 
+
+if __name__ == '__main__':
+    args = parse_input_args()
+    for file in args.filename_in:
+        process_file(file, args)
